f37d5125384650c643f9e06faaac26711707e0ba
ameenelnaggar <ameenelnaggar9@gmail.com>
2026-04-20 07:06:35 +0200

    add partners data

---
+++ src/data/partners.data.js
@@ -0,0 +1,10 @@
+export const partners = [
+  { id: 1, name: "Emaar Properties", logo: "https://placehold.co/160x64/f5f5f0/222?text=EMAAR" },
+  { id: 2, name: "DAMAC Properties", logo: "https://placehold.co/160x64/f5f5f0/222?text=DAMAC" },
+  { id: 3, name: "Nakheel", logo: "https://placehold.co/160x64/f5f5f0/222?text=NAKHEEL" },
+  { id: 4, name: "Meraas", logo: "https://placehold.co/160x64/f5f5f0/222?text=MERAAS" },
+  { id: 5, name: "Aldar Properties", logo: "https://placehold.co/160x64/f5f5f0/222?text=ALDAR" },
+  { id: 6, name: "Sobha Realty", logo: "https://placehold.co/160x64/f5f5f0/222?text=SOBHA" },
+  { id: 7, name: "Azizi Developments", logo: "https://placehold.co/160x64/f5f5f0/222?text=AZIZI" },
+  { id: 8, name: "Dubai Properties", logo: "https://placehold.co/160x64/f5f5f0/222?text=DUBAI+PROP" },
+];
